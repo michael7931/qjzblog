82e16abe978cb76fb732e15e5de3d3b877ba0efd
michael7931 <337994425@qq.com>
2018-02-19 02:41:44 +0800

    .

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -49,14 +49,14 @@
                         <a href="{{ url('category/'.$v->id) }}" onclick="return recordId('cid', '{{ $v->id }}')">{{ $v->name }}</a>
                     </li>
                 @endforeach
-                <li class="b-nav-cname @if($category_id == 'chat') b-nav-active @endif">
-                <a href="{{ url('chat') }}">随言碎语</a>
-                </li>
-                @if(!$gitProject->isEmpty())
-                    <li class="b-nav-cname hidden-sm  @if($category_id == 'git') b-nav-active @endif">
-                    <a href="{{ url('git') }}">开源项目</a>
-                    </li>
-                @endif
+                {{--<li class="b-nav-cname @if($category_id == 'chat') b-nav-active @endif">--}}
+                {{--<a href="{{ url('chat') }}">随言碎语</a>--}}
+                {{--</li>--}}
+                {{--@if(!$gitProject->isEmpty())--}}
+                    {{--<li class="b-nav-cname hidden-sm  @if($category_id == 'git') b-nav-active @endif">--}}
+                    {{--<a href="{{ url('git') }}">开源项目</a>--}}
+                    {{--</li>--}}
+                {{--@endif--}}
             </ul>
             <ul id="b-login-word" class="nav navbar-nav navbar-right">
                 @if(empty(session('user.name')))
